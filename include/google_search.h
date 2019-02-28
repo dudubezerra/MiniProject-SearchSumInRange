70817e99d62fc63228da46c6940b82a78e3bbe7d
José Eduardo Bezerra <42437539+dudubezerra@users.noreply.github.com>
2019-02-28 17:30:17 -0300

    Add doxygen comments

--- include/google_search.h
+++ include/google_search.h
@@ -1,12 +1,24 @@
 #ifndef GOOGLE_SEARCH
 #define GOOGLE_SEARCH
 
-// Contém a resposta da função de busca pelo par que corresponde a soma.
+/*!
+   Struct that contains the answer of the search function
+*/
 struct Result {
-    bool found; //!< Has found the pair.
-    int first; //!< First element of the pair that corresponds to the requested sum.
-    int second; //!< Secon element of the pair (see above).
+    bool found; /*!< Has found the pair. */
+    int first; /*!< First element of the pair that corresponds to the requested sum. */
+    int second; /*!< Secon element of the pair (see above). */
 };
+
+
+/*!
+ *@brief Search pair for a sum function 
+ *@param *first Adress of the first element in the range
+ *@param *last Adress of memory just past the last element in the range 
+ *@param sum Value to be compared with the pair in the range
+ *@return A struct with a boolean, the index of the first element of the pair, and the index of the second element.
+
+ */
 
 Result google_search_pair ( int *first, int *last, int sum);
 
